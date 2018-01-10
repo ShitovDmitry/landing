6cf6943e1ef9f3916ed70bc2c4ac2d9e1f697aa7
ds <ds@combeep.team>
2018-01-10 20:55:33 +0300

    1

---
+++ system/config.php
@@ -0,0 +1,13 @@
+<?
+/**
+* Настройки проекта
+*/
+return [
+	"database" => [
+	"host" => "mysql.hostinger.ru",
+	"database" => "u871500725_vorse",
+	"user" => "u871500725_vor",
+	"password" => "EGtbkj5T"
+],
+"display_errors" => true,
+];
